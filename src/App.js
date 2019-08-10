5bffc41fea2f5dc0df15bd329359f335fd820b8e
hugginssd <hugginsmafigu@gmail.com>
2019-08-10 16:51:37 +0200

    Added searchbar and videodetail to app.js

--- src/App.js
+++ src/App.js
@@ -2,30 +2,37 @@ import React from 'react';
 
 import { Grid } from '@material-ui/core'
 
+import SearchBar from './components';
+import VideoDetail from './components/'
+
 import youtube from '.api/youtube';
 
 class App extends React.Component {
-        render() {
-                return ( <
-                        Grid justify = "center"
-                        container spacing = { 16 } >
-                        <
-                        Grid item xs = { 12 } >
-                        <
-                        Grid container spacing = { 16 } >
-                        <
-                        Grid item xs = { 12 } > { /* SEARCH BAR*/ } <
-                        /Grid> <
-                        Grid item xs = { 8 } > {
-                            /* SEARCH BAR/}
-                                                        </Grid>
-                                                        <Grid item xs={8}>
-                                                            {/* SEARCH BAR/}
-                                                        </Grid>
-                                                    </Grid>
-                                                </Grid>
-                                        </Grid>
-                                    )
-                                }
-                            }
-                            export default App;
+    render() {
+        return ( <
+            Grid justify = "center"
+            container spacing = { 16 } >
+            <
+            Grid item xs = { 12 } >
+            <
+            Grid container spacing = { 16 } >
+            <
+            Grid item xs = { 12 } >
+            <
+            SearchBar / >
+            <
+            /Grid> <
+            Grid item xs = { 8 } >
+            <
+            VideoDetail / >
+            <
+            /Grid> <
+            Grid item xs = { 4 } > { /* VIDEO LIST */ } <
+            /Grid> <
+            /Grid> <
+            /Grid> <
+            /Grid>
+        )
+    }
+}
+export default App;
