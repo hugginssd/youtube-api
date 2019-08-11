b525b772dc795e153dce9fec2739438f6a8d3dc0
hugginssd <hugginsmafigu@gmail.com>
2019-08-11 15:12:37 +0200

    VideoDetail modifications

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -17,7 +17,8 @@ const VideoDetail = ({ video }) => {
         <
         Paper elevation = { 6 }
         style = {
-            { height: '10%' } } >
+            { height: '10%' }
+        } >
         <
         iframe frameBorder = "0"
         height = "100%"
@@ -26,16 +27,18 @@ const VideoDetail = ({ video }) => {
         src = { videoSrc } >
 
         <
-        /iframe>  <
-        /Paper>  <
+        /iframe>  < /
+        Paper > <
         Paper elevation = { 6 }
         style = {
-            { padding: '15px' } } >
+            { padding: '15px' }
+        } >
         <
         Typography variant = "h4" > { video.snippet.title } - { video.snippet.channelTitle } < /Typography>  <
         Typography variant = "subtitle1" > { video.snippet.channelTitle } < /Typography> <
-        Typography variant = "subtitle2" > { video.snippet.description } < /Typography>  <
-        /Paper>  <
+        Typography variant = "subtitle2" > { video.snippet.description } < /Typography>  < /
+        Paper >
+        <
         /React.Fragment>
 
     )
