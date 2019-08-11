06a01e47e043d19330bae8acc8e156b2d7bbb0a1
hugginssd <hugginsmafigu@gmail.com>
2019-08-11 15:15:35 +0200

    Updates on components

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -28,16 +28,17 @@ const VideoDetail = ({ video }) => {
 
         <
         /iframe>  < /
-        Paper > <
+        Paper >
+        <
         Paper elevation = { 6 }
         style = {
             { padding: '15px' }
         } >
         <
-        Typography variant = "h4" > { video.snippet.title } - { video.snippet.channelTitle } < /Typography>  <
-        Typography variant = "subtitle1" > { video.snippet.channelTitle } < /Typography> <
-        Typography variant = "subtitle2" > { video.snippet.description } < /Typography>  <
-        /Paper > <
+        Typography variant = "h4" > { video.snippet.title } - { video.snippet.channelTitle } < /Typography>   <
+        Typography variant = "subtitle1" > { video.snippet.channelTitle } < /Typography>  <
+        Typography variant = "subtitle2" > { video.snippet.description } < /Typography>   < /
+        Paper > <
         /React.Fragment>
 
     )
